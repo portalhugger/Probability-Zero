b2f3b4b50788962edeee711fe5a2e4766482faaf
portalhugger <nathaniel2001@gmail.com>
2022-06-30 14:33:45 -0400

    a

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
 var gameData = {
-    uni: 0,
-    accuracy: 0,
-    gen: 1,
-    genCost: 10
+  uni: 0,
+  accuracy: 0,
+  gen: 1,
+  genCost: 10
   }
 
-  function genUni() {
-    gameData.uni += gameData.gen,
+function openTab(evt, tabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("function");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" tabSelected", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " tabSelected";
+}
+
+/// TAB 1: COMPUTER TERMINAL
+
+function genUni() {
+  gameData.uni += gameData.gen,
+  document.getElementById("uniScore").innerHTML = gameData.uni + " universes generated"
+}
+
+function buyGen() {
+  if (gameData.uni >= gameData.genCost) {
+    gameData.uni -= gameData.genCost
+    gameData.gen += 1
+    gameData.genCost += 10
+    document.getElementById("genUniButton").innerHTML = `» generate ${gameData.gen} random universes «`
     document.getElementById("uniScore").innerHTML = gameData.uni + " universes generated"
   }
+}
 
-  function buyGen() {
-    if (gameData.uni >= gameData.genCost) {
-      gameData.uni -= gameData.genCost
-      gameData.gen += 1
-      gameData.genCost += 10
-      document.getElementById("genUniButton").innerHTML = `» generate ${gameData.gen} random universes «`
-      document.getElementById("uniScore").innerHTML = gameData.uni + " universes generated"
-    }
-  }
-
-  function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("function");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" tabSelected", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " tabSelected";
-  }
-
-  
+///TAB 2: 
