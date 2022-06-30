1d088e9fd4c6634a948a24f234f1a2ab3bce8634
portalhugger <nathaniel2001@gmail.com>
2022-06-29 22:34:27 -0400

    test

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 var gameData = {
     uni: 0,
     accuracy: 0,
-    uniPerGen: 1
+    gen: 1,
+    genCost: 10
   }
 
   function genUni() {
-    gameData.uni += gameData.uniPerGen,
+    gameData.uni += gameData.gen,
     document.getElementById("uniScore").innerHTML = gameData.uni + " universes generated"
   }
+
+  function buyGen() {
+    if (gameData.uni >= gameData.genCost) {
+      gameData.uni -= gameData.genCost
+      gameData.gen += 1
+      gameData.genCost += 10
+      document.getElementById("genUniButton").innerHTML = `» generate ${gameData.gen} random universes «`
+      document.getElementById("uniScore").innerHTML = gameData.uni + " universes generated"
+    }
+  }
+  
